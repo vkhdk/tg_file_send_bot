1388612ad62bab841729ead1f3f5454402f26ff5
Vova <vkhdk@yandex.ru>
2022-11-24 18:07:44 +0300

    add chat_id_data

--- bot.py
+++ bot.py
@@ -1,31 +1,34 @@
-#importing public libraries
+# importing public libraries
 import datetime as dt
 from datetime import datetime
 import logging
 import pandas as pd
 import json
 
-#importing library from pyTelegramBotAPI
+# importing library from pyTelegramBotAPI
 import telebot
-#library for keyboard settings
+# library for keyboard settings
 from telebot import types
 
-#importing internal modules
+# importing internal modules
 import secrets
 
-#initializing the bot
-#store token in file secrets.py
+# initializing the bot
+# store token in file secrets.py
 bot = telebot.TeleBot(secrets.bot_token)
 
-@bot.message_handler(chat_types = ['private'], commands = ['start'])
-def start_keyboard(message):
-    kb = types.ReplyKeyboardMarkup(resize_keyboard = True)
-    btn1 = types.KeyboardButton(text = '👤 Авторизация')
-    btn2 = types.KeyboardButton(text = '📖 Инструкция')
-    kb.add(btn1,btn2)
-    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}!', reply_markup = kb)
 
-@bot.message_handler(chat_types = ['private'], content_types = ['text'])
+@bot.message_handler(chat_types=['private'], commands=['start'])
+def start_keyboard(message):
+    kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton(text='👤 Авторизация')
+    btn2 = types.KeyboardButton(text='📖 Инструкция')
+    kb.add(btn1, btn2)
+    bot.send_message(
+        message.chat.id, f'Привет, {message.from_user.first_name}!', reply_markup=kb)
+
+
+@bot.message_handler(chat_types=['private'], content_types=['text'])
 def keyboard_navigation(message):
     if message.text == '📖 Инструкция':
         bot.send_message(message.chat.id, (f'Бот создан для отправки файлов за указанную дату.\n'
@@ -36,26 +39,41 @@ def keyboard_navigation(message):
                                                  f'Введите ЛОГИН'))
         bot.register_next_step_handler(msg, check_login)
 
+
 def check_login(message):
     user_login = message.text
     authorization_data_full = authorization_data()
-    authorization_data_current = authorization_data_full[authorization_data_full['login'] == user_login]
-    if len(authorization_data_current)>0:
+    authorization_data_current = authorization_data_full[
+        authorization_data_full['login'] == user_login]
+    if len(authorization_data_current) > 0:
+        chat_id_data_full = chat_id_data()
+        chat_id_data_current = chat_id_data_full[chat_id_data_full['login'] == user_login]
         msg = bot.send_message(message.chat.id, (f'Введите ПАРОЛЬ'))
         bot.register_next_step_handler(msg, check_password)
-    else: 
-        bot.send_message(message.chat.id, (f'Пользователь с логином {user_login} не найден'))
+    else:
+        bot.send_message(
+            message.chat.id, (f'Пользователь с логином {user_login} не найден'))
+
 
 def check_password(message):
     bot.send_message(message.chat.id, (f'Пока тут все!'))
 
 
-
-
-#read authorization data
+# read authorization data
 def authorization_data():
-    link_authorization_data = secrets.authorization_data_link + secrets.authorization_data_name
+    link_authorization_data = secrets.authorization_data_link + \
+        secrets.authorization_data_name
     authorization_data = pd.read_csv(f'{link_authorization_data}.csv', sep=',')
     return authorization_data
+
+# read chat id data
+def chat_id_data():
+    link_chat_id_data = secrets.chat_id_data_link + \
+        secrets.chat_id_data_name
+    chat_id_data = pd.read_csv(f'{link_chat_id_data}.csv', sep=',')
+    return chat_id_data
+
+# write current user info
+
 
 bot.polling()
--- get_data_from_goolge_docs.py
+++ get_data_from_goolge_docs.py
@@ -1,15 +1,16 @@
-#script to download data from google doc to csv file
-#secrets.py contains data about the key on a google spreadsheet
-#example url https://docs.google.com/spreadsheets/d/1-GZ36cIpx1oIjiZ4hCZ1L6JieierCfJA4rsK36Lbe0E 
-#where "1-GZ36cIpx1oIjiZ4hCZ1L6JieierCfJA4rsK36Lbe0E" is the key
+# script to download data from google doc to csv file
+# secrets.py contains data about the key on a google spreadsheet
+# example url https://docs.google.com/spreadsheets/d/1-GZ36cIpx1oIjiZ4hCZ1L6JieierCfJA4rsK36Lbe0E
+# where "1-GZ36cIpx1oIjiZ4hCZ1L6JieierCfJA4rsK36Lbe0E" is the key
 
-#importing public libraries
+# importing public libraries
 import pandas as pd
 from io import BytesIO
 import requests
 
-#importing internal modules
+# importing internal modules
 import secrets
+
 
 def down_load_google_doc(doc_key):
     google_doc_url = 'https://docs.google.com/spreadsheets/d/'
@@ -19,13 +20,26 @@ def down_load_google_doc(doc_key):
     df = pd.read_csv(BytesIO(data), index_col=0)
     return(df)
 
+
 def save_df_to_csv(df, link, file_name):
     df.to_csv(link + f'{file_name}.csv')
 
-if __name__ == '__main__':
-    doc_key = secrets.google_doc_key
-    link = secrets.authorization_data_link
-    file_name = secrets.authorization_data_name
 
-    df = down_load_google_doc(doc_key)
-    save_df_to_csv(df, link, file_name)
+if __name__ == '__main__':
+    google_authorization_data_key = secrets.google_authorization_data_key
+    authorization_data_link = secrets.authorization_data_link
+    authorization_data_name = secrets.authorization_data_name
+
+    df_authorization_data = down_load_google_doc(google_authorization_data_key)
+    save_df_to_csv(df_authorization_data,
+                   authorization_data_link,
+                   authorization_data_name)
+
+    google_chat_id_data_key = secrets.google_chat_id_data_key
+    chat_id_data_link = secrets.chat_id_data_link
+    chat_id_data_name = secrets.chat_id_data_name
+
+    df_chat_id_data = down_load_google_doc(google_chat_id_data_key)
+    save_df_to_csv(df_chat_id_data,
+                   chat_id_data_link,
+                   chat_id_data_name)
